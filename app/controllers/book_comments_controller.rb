f351b1934033b6efc70782918246dd149860046b
geruharuto <geruharuto2121@yahoo.co.jp>
2020-06-03 14:18:03 +0900

    コメント、いいね実装完了

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,17 +1,16 @@
 class BookCommentsController < ApplicationController
 
 	def create
-	 	@book = Book.find(params[:book_id])
-	 	@comment = @book.book_comments.new(comment_params)
+	 	@book_find = Book.find(params[:book_id])
+	 	@comment = @book_find.book_comments.new(comment_params)
 	 	@comment.user_id = current_user.id #同じ変数を使い、足りない情報を追加している
+	 	@comments = BookComment.where(book_id: @book_find.id)
 	 	if @comment.save
-	 	   redirect_to request.referer
+	 	   redirect_to book_path(@book_find.id)
 	 	else
-	 	   @book_find = Book.find(params[:id])
-	 	   render template: "books/show"
-	 	   @user = User.find_by(id: @book_find.user_id)
-           @book = Book.new
-           @comments = @book_find.book_comments
+	 	   @user = User.find(@book_find.user_id)
+	 	   @book = Book.new
+           render template: "books/show"
 	 	end
 	end
 
@@ -21,10 +20,10 @@ class BookCommentsController < ApplicationController
            @comment.destroy
 	       redirect_to request.referer
         else
-	       @book_find = Book.find(params[:id])
-           @user = User.find_by(id: @book_find.user_id)
-           @book = Book.new
-           @comments = @book_find.book_comments
+           @book_find = Book.find(params[:book_id])
+           @user = User.find(@book_find.user_id)
+	 	   @book = Book.new
+	 	   @comments = @book_find.book_comments
            render template: "books/show"
 	    end
     end
